658560bd8ca5d05b8fe40727f219089fa9ed1b28
Michael Landis <mlandis@gmail.com>
2017-03-29 18:02:52 -0400

    testing possible interference betw linenoise and metakernel.REPLWrapper

--- test_repl.py
+++ test_repl.py
@@ -1,13 +1,23 @@
 from metakernel import REPLWrapper
 
-x = REPLWrapper(cmd_or_spawn='/usr/local/bin/rb',
+# works with ``#define RB_XCODE``, i.e. no linenoise
+cmd = '/usr/local/bin/rb'         
+
+# possibly broken with the cmake version
+# cmd = '/usr/local/bin/revbayes'
+
+
+x = REPLWrapper(cmd_or_spawn=cmd,
                 prompt_regex=r'[>+] ',
                 prompt_change_cmd=None,
                 echo=True)
 
-x.run_command('x <- [1,')
+# RevBayes uses u'\n\r' as CRLF
+# x.child.crlf = '\n'
+
+x.run_command('val <- [1,')
 x.run_command('2]')
-x.run_command('x')
+x.run_command('val')
 
 #print(x.run_command('for (i in 1:10) z[i] ~ dnNormal(0,1)'))
 #print(x.run_command('z'))
